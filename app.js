c3033800e68af50977de879d97c205f52396b3f5
Tyron Aricun <tyronaricum1@gmail.com>
2023-03-11 10:21:45 +0200

    update views and implement CRUD with monogoDB

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 // jshint esversion: 6
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
+// const date = require(__dirname + '/date.js');
 
 const app = express();
 
-const items = [
-    "Buy Food",
-    "Cook Food",
-    "Eat Food"
-];
+main().catch(err => console.log(err));
 
-const workItems = [
-    "Food",
-    "Stuff",
-];
+async function main() {
+    mongoose.connect("mongodb://127.0.0.1:27017/todoListDB", { useNewUrlParser: true });
+    console.log("Connected successfully to server");
+    
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const itemsSchema = new mongoose.Schema({
+    name: String,
+});
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({
+    name: 'Welcome to your todo list!'
+});
+const item2 = new Item({
+    name: 'Hi tthe + button to add a new item.'
+});
+const item3 = new Item({
+    name: '<--- Hit this to delete an item.'
+});
+
+const defaultItems = [item1, item2, item3];
+
+
 
 app.set('view engine', 'ejs');
 
@@ -22,22 +41,65 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    let day = date.getDate();
+    // let day = date.getDate();
 
-    res.render('list', {listTitle: day, newListItems: items});
+    Item.find({})
+    .then(function(items) {
+        // res.render('list', {listTitle: day, newListItems: items});
+
+        if(items.length === 0) {
+            Item.insertMany(defaultItems)
+                .then(function () {
+                    console.log("successfully saved dafault items to db");
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+
+            // redirect - root
+            res.redirect("/");
+        } else {
+
+            // render
+            res.render('list', {listTitle: "Today", newListItems: items});
+        }
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+
 
 });
 
 app.post("/", (req, res) => {
-    let item = req.body.newItem;
+    let itemName = req.body.newItem;
 
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect('/work');
-    } else {
-        items.push(item);
-        res.redirect('/');
-    }
+    const item = new Item({
+        name: itemName
+    });
+
+    item.save();
+
+    res.redirect("/")
+
+});
+
+app.post("/delete", (req, res) => {
+    console.log((req.body.checkbox));
+    let checkedItemId = req.body.checkbox;
+
+    const item = new Item({
+        _id: checkedItemId
+    });
+
+    Item.findByIdAndRemove(checkedItemId)
+    .then(function (res) {
+        console.log("succesfully deleted item");  
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+    res.redirect("/")
 
 });
 
